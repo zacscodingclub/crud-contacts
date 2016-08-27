99e29d4c4d08f86ffae89bc7ac140cf8a1d3aa8e
zacscodingclub <zbaston@gmail.com>
2016-08-27 11:27:54 -0500

    add edit contact and remove contact functionality

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -16,25 +16,28 @@ angular
 
     vm.contacts = $firebaseArray(ref);
 
-    $scope.showAddForm = function() {
-      $scope.addFormShow = true;
+    $scope.hide = function() {
+      $scope.addFormShowFlag = false;
+      $scope.contactShowFlag = false;
+      $scope.editFormShowFlag = false;
     }
 
-    $scope.hideAddForm = function() {
-      $scope.addFormShow = false;
+// Add Contact Functioniality
+    $scope.showAddForm = function() {
+      $scope.addFormShowFlag = true;
     }
 
     $scope.addContactSubmit = function() {
-      var name = $scope.name || null;
-      var email = $scope.email || null;
-      var company = $scope.company || null;
-      var mobile_phone = $scope.mobile_phone || null;
-      var work_phone = $scope.work_phone || null;
-      var home_phone = $scope.home_phone || null;
+      var name           = $scope.name || null;
+      var email          = $scope.email || null;
+      var company        = $scope.company || null;
+      var mobile_phone   = $scope.mobile_phone || null;
+      var work_phone     = $scope.work_phone || null;
+      var home_phone     = $scope.home_phone || null;
       var street_address = $scope.street_address || null;
-      var city = $scope.city || null;
-      var state = $scope.state || null;
-      var zipcode = $scope.zipcode || null;
+      var city           = $scope.city || null;
+      var state          = $scope.state || null;
+      var zipcode        = $scope.zipcode || null;
 
       vm.contacts.$add({
         name: name,
@@ -56,11 +59,80 @@ angular
           }
         ]
       }).then(function(ref) {
+
         $scope.newContactForm.$setUntouched();
         $scope.newContactForm.$setPristine();
 
-        $scope.addFormShow = false;
+        $scope.addFormShowFlag = false;
         vm.msg = "Your contact was added.";
       })
+    }
+
+// Edit Contact
+    $scope.showEditForm = function(contact) {
+      $scope.editFormShowFlag = true;
+
+      $scope.id             = contact.$id
+      $scope.name           = contact.name;
+      $scope.company        = contact.company;
+      $scope.email          = contact.email;
+      $scope.mobile_phone   = contact.phones[0].mobile || null;
+      $scope.work_phone     = contact.phones[0].work || null;
+      $scope.home_phone     = contact.phones[0].home || null;
+      $scope.street_address = contact.address[0].street_address || null;
+      $scope.city           = contact.address[0].city || null;
+      $scope.state          = contact.address[0].state || null;
+      $scope.zipcode        = contact.address[0].zipcode || null;
+    }
+
+    $scope.editContactSubmit = function() {
+
+      var id = $scope.id
+      var record = vm.contacts.$getRecord(id);
+
+      record.name                      = $scope.name;
+      record.email                     = $scope.email;
+      record.company                   = $scope.company;
+      record.phones[0].mobile          = $scope.mobile_phone;
+      record.phones[0].work            = $scope.work_phone;
+      record.phones[0].home            = $scope.home_phone;
+      record.address[0].street_address = $scope.street_address;
+      record.address[0].city           = $scope.city;
+      record.address[0].state          = $scope.state;
+      record.address[0].zipcode        = $scope.zipcode;
+
+      vm.contacts.$save(record)
+        .then(function(){
+          $scope.editContactForm.$setUntouched();
+          $scope.editContactForm.$setPristine();
+
+          $scope.hide();
+          $scope.msg = "Contact Updated";
+        })
+
+    }
+
+// Remove Contact
+
+    $scope.removeContact = function(contact) {
+      vm.contacts.$remove(contact);
+      $scope.msg = "Contact Removed";
+    }
+
+// Show Contact Functionality
+
+    $scope.showContact = function(contact) {
+      $scope.name           = contact.name;
+      $scope.company        = contact.company;
+      $scope.email          = contact.email;
+      $scope.mobile_phone   = contact.phones[0].mobile || null;
+      $scope.work_phone     = contact.phones[0].work || null;
+      $scope.home_phone     = contact.phones[0].home || null;
+      $scope.street_address = contact.address[0].street_address || null;
+      $scope.city           = contact.address[0].city || null;
+      $scope.state          = contact.address[0].state || null;
+      $scope.zipcode        = contact.address[0].zipcode || null;
+
+      $scope.contactShowFlag = true;
     }
   }]);
