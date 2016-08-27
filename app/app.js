f60a5cae0d9d8a70046a775c35dba0a00941da3a
zacscodingclub <zbaston@gmail.com>
2016-08-27 08:46:38 -0500

    clean up app.js file, include firebase and contacts nested view

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@
 
 // Declare app level module which depends on views, and components
 angular.module('crudContacts', [
-  'ngRoute'
+  'ngRoute',
+  'firebase',
+  'crudContacts.contacts'
 ]).
 config(['$routeProvider', function( $routeProvider) {
   $routeProvider.otherwise({redirectTo: '/contacts'});
