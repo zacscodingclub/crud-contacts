b5e219c70a73e675eae2d78b490dad04b9d48107
zacscodingclub <zbaston@gmail.com>
2016-08-27 09:01:38 -0500

    update gitinore rules

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -2,12 +2,17 @@
 
 angular
   .module('crudContacts.contacts', ['ngRoute', 'firebase'])
+
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/contacts', {
       templateUrl: 'contacts/contacts.html',
       controller: 'ContactsController'
     })
   }])
+
   .controller('ContactsController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-    console.log($scope);
+    var ref = new Firebase('https://crud-contacts.firebaseio.com/contacts');
+
+    $scope.contacts = $firebaseArray(ref);
+    console.log($scope.contacts);
   }]);
