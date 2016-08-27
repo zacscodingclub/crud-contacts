60cf664f35911be9e398b0660895f93a492300dc
zacscodingclub <zbaston@gmail.com>
2016-08-27 08:47:14 -0500

    build out basic contacts.js with route, DI, and contacts.html structure

---
+++ app/contacts/contacts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular
+  .module('crudContacts.contacts', ['ngRoute', 'firebase'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/contacts', {
+      templateUrl: 'contacts/contacts.html',
+      controller: 'ContactsController'
+    })
+  }])
+  .controller('ContactsController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+    console.log($scope);
+  }]);
